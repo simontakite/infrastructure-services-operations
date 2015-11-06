e63a7b22df63a19ddc8cda0396a5559d1efb6314
Simon Takite <simon.takite@gmail.com>
2015-11-06 13:13:34 +0100

    backup runner init

---
+++ backuprunner/manifests/init.pp
@@ -0,0 +1,40 @@
+# == Class: backuprunner
+#
+# Full description of class backuprunner here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'backuprunner':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domdirectories>
+#
+# === Copyright
+#
+# Copyright 2015 Your name here, unless otherwise noted.
+#
+class backuprunner {
+    directories => [ '/var/www/', '/var/lib/mysql' ],
+}
